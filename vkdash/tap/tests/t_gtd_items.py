eb16e02f71bdc14a8906549f8ff20ffe63de126e
John (EBo) David <ebo@users.sourceforge.net>
2019-02-16 06:15:48 -0500

    changed the skip/todo output directive to all upper case for clarity. Also added tests to make sure that the todo/skip directives are parsed case insensitive.

--- vkdash/tap/tests/t_gtd_items.py
+++ vkdash/tap/tests/t_gtd_items.py
@@ -37,12 +37,12 @@ itm.parse(orig_str)
 itm_str = str(itm)
 plan.ok(expect_str==itm_str, "Test 'not ok' with missing '-' and converted back to a string.")
 
-orig_str = "ok 1 - testing # skip something"
+orig_str = "ok 1 - testing # SKIP something"
 itm.parse(orig_str)
 itm_str = str(itm)
 plan.ok(orig_str==itm_str, "Test 'ok' with skip and converted back to a string.")
 
-orig_str = "not ok 1 - testing # todo something else"
+orig_str = "not ok 1 - testing # TODO something else"
 itm.parse(orig_str)
 itm_str = str(itm)
 plan.ok(orig_str==itm_str, "Test 'not ok' with todo and converted back to a string.")
@@ -58,17 +58,23 @@ plan.ok(itm.type()=="pass", "testing item type directly")
 plan.ok(itm=="pass", "testing item type infered")
 plan.ok(itm=="PaSs", "testing item camilcase type")
 
-orig_str = "ok 123 - a test # skip why are we skipping?"
+orig_str = "ok 123 - a test # SKIP why are we skipping?"
 itm.parse(orig_str)
 itm_str = str(itm)
 plan.ok(orig_str==itm_str, "Testing skip directive + some comments.")
-
 
 orig_str = "ok 456 - a test # some comments..."
 itm.parse(orig_str)
 itm_str = str(itm)
 plan.ok(orig_str==itm_str, "Testing comments in the directive filed.")
 
+orig_str = "ok 923 - test cased skip # Skip just to be sure"
+itm.parse(orig_str)
+plan.ok(itm.itype=="skip", "Testing case Skip directive + some comments.")
+
+orig_str = "ok 924 - test cased skip # ToDo just to be sure"
+itm.parse(orig_str)
+plan.ok(itm.itype=="todo", "Testing case ToDo directive + some comments.")
 
 results = os.path.splitext(__file__)[0]+".tap"
 fout = open(results,'w')
